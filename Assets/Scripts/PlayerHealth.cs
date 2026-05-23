b56744e8a096bac5e9edd29c931fb4a84b12ed5d
Erica Gan <ericagance@gmail.com>
2026-05-23 15:33:54 +0800

    Fix: Applied official Unity gitignore rules

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Health : MonoBehaviour
+{
+    public int curHealth = 0;
+    public int maxHealth = 100;
+
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        curHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if( Input.GetKeyDown( KeyCode.Space ) )
+        {
+            DamagePlayer(10);
+        }
+    }
+
+    public void DamagePlayer( int damage )
+    {
+        curHealth -= damage;
+
+        healthBar.SetHealth( curHealth );
+    }
+}
