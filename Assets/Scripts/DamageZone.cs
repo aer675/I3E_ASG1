8963aed1b45d805a8e5652f8b546f162e8d98bba
Aerica <ericagance@gmail.com>
2026-06-08 16:58:14 +0800

    14 Added trigger zone that damage Player 1 damage per second when they enter the zone

--- Assets/Scripts/DamageZone.cs
+++ Assets/Scripts/DamageZone.cs
@@ -1,16 +1,33 @@
+/*
+ * Author: Aerica Gan Chai Ting
+ * Date: 8 June 2026
+ * Description: Manages damage zones, applying damage to the player when they enter the zone.
+ */
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    /// Applies damage to the player when they stay within the damage zone.
+    /// </summary>
+    /// <param name="other">The collider that is staying within the damage zone.</param>
+    void OnTriggerStay(Collider other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
+            if(playerHealth != null)
+            {
+                playerHealth.TakeDamage(1); // Apply 1 damage per second
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerExit(Collider other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            print("Player exited damage zone, no more damage applied.");
+        }
     }
 }
