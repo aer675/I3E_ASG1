14e742ba2f7b3fe37e988fb93db114cb8ec004fe
Aerica <ericagance@gmail.com>
2026-06-09 16:58:33 +0800

    29 - Game manager skeleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,74 @@
 /*
  * Author: Aerica Gan Chai Ting
- * Date: 8 June 2026
- * Description: Manages the game state and overall game logic.
+ * Date: 9 June 2026
+ * Description: Core manager handling game state, inventory tracking, score, and win/lose conditions.
  */
 
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary> Singleton instance allowing global access to the GameManager. </summary>
+    public static GameManager Instance { get; private set; }
+
+    [Header("Inventory & Score Tracking")]
+    /// <summary> Tracks the total number of map fragments the player has collected. </summary>
+    public int mapCount = 0;
+
+    /// <summary> Tracks the player's total score from collecting items. </summary>
+    public int score = 0;
+
+    [Header("Security Clearance")]
+    /// <summary> Flag indicating if the player has unlocked Level 1 door access. </summary>
+    public bool hasLevel1Card = false;
+
+    /// <summary> Flag indicating if the player has unlocked Level 2 door access. </summary>
+    public bool hasLevel2Card = false;
+
+    /// <summary>
+    /// Initializes the Singleton instance when the script loads.
+    /// Ensures only one GameManager ever exists in the scene.
+    /// </summary>
+    void Awake()
     {
-        
+        // Singleton setup logic
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Increments the map fragment count and checks if the Level 1 keycard should spawn.
+    /// </summary>
+    public void AddMapFragment()
     {
+        mapCount++;
+        print("Map fragment collected! Total maps: " + mapCount);
         
+        // TODO: Update your Map UI counter here!
+        
+        if (mapCount >= 5)
+        {
+            hasLevel1Card = true;
+            print("Level 1 Keycard Authorized!");
+            // TODO: Trigger a UI message or sound effect here!
+        }
+    }
+
+    /// <summary>
+    /// Adds points to the player's total score.
+    /// </summary>
+    /// <param name="points">The amount of points to award the player.</param>
+    public void AddScore(int points)
+    {
+        score += points;
+        print("Score updated! Current score: " + score);
+        
+        // TODO: Update your Score UI text here!
     }
 }
